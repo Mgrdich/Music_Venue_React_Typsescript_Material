49b5335829682cb0d2521e275668112128ba9fd5
Mgrdich <mgotm13@gmail.com>
2019-09-22 22:17:32 +0400

    clean up no sass

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,11 @@
-import * as React from 'react';
-import './App.css';
+import React from 'react';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            Hello
+        </div>
     );
-  }
-}
+};
 
 export default App;
