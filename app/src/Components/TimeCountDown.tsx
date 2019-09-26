6f3eca6c389774ec6f8c193187b6b5650c768c83
Mgrdich <mgotm13@gmail.com>
2019-09-27 01:38:57 +0400

    Clock timing

--- app/src/Components/TimeCountDown.tsx
+++ app/src/Components/TimeCountDown.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Deadline, myDate} from "../interfaces/CompInterfaces";
+
+let Slide = require("react-reveal/Slide");
 
 const TimeCountDown: React.FC = () => {
+    const deadline = 'Dec, 18, 2020';
+    const [myDate, ChangeData] = useState<myDate>({});
+
+    useEffect(() => {
+        setInterval(() => getTimeUntil(deadline), 1000);
+    }, [deadline]);
+
+    function getTimeUntil(deadline: string) {
+        const time = Date.parse(deadline) - Date.parse(new Date().toString());
+        if (time < 0) {
+
+        } else {
+            const seconds: number = Math.floor((time / 1000) % 60);
+            const minutes: number = Math.floor((time / 1000 / 60) % 60);
+            const hours: number = Math.floor(time / (1000 * 60 * 60) % 24);
+            const days: number = Math.floor((time / (1000 * 60 * 60 * 24)));
+            ChangeData({seconds, minutes, hours, days})
+        }
+    }
 
     return (
-        <div className="countdown">
-            <div className="countdown_top">
-                Concert Start In
-            </div>
-            <div className="countdown_bottom">
-                <div className="countdown_time">
-                    23
+        <Slide left delay={1000}>
+            <div className="countdown">
+                <div className="countdown_top">
+                    Concert Start In
                 </div>
-                <div className="countdown_tag">
-                    Days
-                </div>
+                {
+                    Object.keys(myDate).map((item) => {
+                        return (
+                            <div className="countdown_bottom">
+                                <div className="countdown_time">
+                                    {item}
+                                </div>
+                                <div className="countdown_tag">
+                                    {myDate.days}
+                                </div>
+                            </div>
+                        );
+                    })
+                }
             </div>
-        </div>
+        </Slide>
     );
 };
 
---
+++ app/src/interfaces/CompInterfaces.ts
@@ -0,0 +1,10 @@
+export interface myDate {
+    days?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+}
+
+export interface Deadline  {
+    readonly deadline?: string
+}
